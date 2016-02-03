34c569cbd4e04d0deeacfa14238c42b84bdacc7e
Erik Öhrn <erik.ohrn@outlook.com>
2016-02-03 14:54:05 +0100

    Added semistateful solution

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -14,22 +14,20 @@ public class Lab1 {
         for (int i = 0; i< semaphores.length; i++) {
             semaphores[i] = new Semaphore(1);
         }
-        Train train1 = new Train(1,speed1);
-        Train train2 = new Train(2,speed2);
+        Train train1 = new Train(1,22);
+        Train train2 = new Train(2,22);
         train1.start();
         train2.start();
 
         while(true){
             try {
                 Thread.sleep(4000);
-                int new1 = ThreadLocalRandom.current().nextInt(1, 15 + 1);
-                int new2 = ThreadLocalRandom.current().nextInt(1, 15 + 1);
-                train1.setTest(new1);
+                int new1 = ThreadLocalRandom.current().nextInt(1, 21 + 1);
+                int new2 = ThreadLocalRandom.current().nextInt(1, 21 + 1);
+            //    train1.setTest(new1);
              //   train2.setTest(new2);
 
-            } catch (CommandException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -49,21 +47,24 @@ public class Lab1 {
         SensorEvent sensor;
         Direction direction;
         ArrayList sensors;
-
+        boolean[] holdingSemaphore;
         private Train(Integer id, Integer speed) {
             this.id = id;
             this.speed = speed;
             tsi = TSimInterface.getInstance();
             setupSensors();
+            holdingSemaphore = new boolean[7];
         }
-
 
         @Override
         public void run() {
             try {
+               // tsi.setDebug(false);
                 direction = id == 1 ? Direction.SOUTH : Direction.NORTH;
-                if(id == 2) semaphores[5].acquire();
-
+                if(id == 2) {
+                    semaphores[5].acquire();
+                    holdingSemaphore[5] = true;
+                }
                 tsi.setSpeed(id, speed);
 
                 while(true){
@@ -80,52 +81,62 @@ public class Lab1 {
                         case 2:case 3:case 4:case 5:
                             if((direction == Direction.NORTH && (index == 2 || index == 3))
                                         || (direction == Direction.SOUTH && (index == 4 || index == 5))) {
-                                semaphores[0].release();
-                            }else{
-                                stopForSemaphore(0);
-                            }
+                                release(0);
+                            }else   stopForSemaphore(0);
                             break;
                         case 6:case 7:
-                            if(direction == Direction.NORTH) {
-                                semaphores[2].release();
-                            }else{
+                            if(direction == Direction.NORTH)
+                                release(2);
+                            else{
                                 stopForSemaphore(2);
-                                if(index == 6) semaphores[1].release();
+                                if(holdingSemaphore[1])
+                                    release(1);
                                 setSwitch(17, 7, index == 6);
-                                setSwitch(15, 9, !semaphores[3].tryAcquire());
                             }
                             break;
                         case 8:case 9:
                             if(direction == Direction.NORTH) {
                                 stopForSemaphore(2);
-                                if(index == 8) semaphores[3].release();
-                                setSwitch(15, 9 , index == 9);
-                                setSwitch(17, 7, semaphores[1].tryAcquire());
-                            }else{
-                                semaphores[2].release();
-                            }
+                                setSwitch(15, 9, index == 9);
+                            }else
+                                release(2);
                             break;
                         case 10:case 11:
-                            if(direction == Direction.NORTH) {
-                                semaphores[4].release();
-                            }else{
+                            if(direction == Direction.NORTH)
+                                release(4);
+                            else{
                                 stopForSemaphore(4);
-                                if (index == 10) semaphores[3].release();
                                 setSwitch(4, 9, index == 10);
-                                setSwitch(3, 11, semaphores[5].tryAcquire());
                             }
                             break;
                         case 12:case 13:
                             if(direction == Direction.NORTH) {
                                 stopForSemaphore(4);
-                                if(index == 13) semaphores[5].release();
+                                if(holdingSemaphore[5])
+                                    release(5);
                                 setSwitch(3, 11, index == 13);
-                                setSwitch(4, 9, semaphores[3].tryAcquire());
-
                             }else{
-                                semaphores[4].release();
+                                release(4);
                             }
                             break;
+                           case 16:
+                                if(direction == Direction.SOUTH) {
+                                    setSwitch(15, 9, !(holdingSemaphore[3] = semaphores[3].tryAcquire()));
+                                }   else {
+                                    if (holdingSemaphore[3])
+                                        release(3);
+                                    setSwitch(17, 7, holdingSemaphore[1] = semaphores[1].tryAcquire());
+                                }
+                                break;
+                            case 17:
+                                if(direction == Direction.NORTH){
+                                    setSwitch(4, 9, holdingSemaphore[3] = semaphores[3].tryAcquire());
+                                } else{
+                                    if (holdingSemaphore[3])
+                                        release(3);
+                                    setSwitch(3, 11, holdingSemaphore[5] = semaphores[5].tryAcquire());
+                                }
+                                break;
                         }
                     }   //End sensor active
                 }   //End while(true)
@@ -140,29 +151,39 @@ public class Lab1 {
             }
         }
 
+        private void release(int index){
+            semaphores[index].release();
+            holdingSemaphore[index] = false;
+        }
+
         private void setSwitch(int x, int y, boolean left) throws CommandException {
             tsi.setSwitch(x, y, left ? tsi.SWITCH_LEFT : tsi.SWITCH_RIGHT);
         }
 
         private void stopForSemaphore(int semaphoreIndex) throws CommandException, InterruptedException {
-            if(semaphores[semaphoreIndex].availablePermits()==0) {
+            if(!semaphores[semaphoreIndex].tryAcquire()){
                 int slow = (int) (Math.min(Math.abs(speed),5) * Math.signum(speed));
                 tsi.setSpeed(id, slow);
+                boolean hasPassedSensor = tsi.getSensor(id).getStatus() == SensorEvent.INACTIVE;
+                if(hasPassedSensor && !semaphores[semaphoreIndex].tryAcquire()){
+                    tsi.setSpeed(id, 0);
+                    semaphores[semaphoreIndex].acquire();
+                }
+                tsi.setSpeed(id, speed);
             }
-            boolean hasPassedSensor = tsi.getSensor(id).getStatus() == SensorEvent.INACTIVE;
-            if(hasPassedSensor && !semaphores[semaphoreIndex].tryAcquire()){
-                tsi.setSpeed(id, 0);
-                semaphores[semaphoreIndex].acquire();
-            }
-            tsi.setSpeed(id, speed);
+            holdingSemaphore[semaphoreIndex] = true;
         }
 
         private void changeDirection() throws CommandException, InterruptedException{
             tsi.setSpeed(id, 0);
-            sleep(Math.min(1000, 1000 * Math.abs(speed)));
+            sleep(1000 + (20 * Math.abs(speed)));
             speed = -speed;
             direction = Direction.opposite(direction);
             tsi.setSpeed(id, speed);
+            if(id == 1){
+                Random r = new Random();
+               speed = (int)Math.signum(speed)*(r.nextInt((22 - 1) + 1) + 1);
+            }
         }
 
         private void setupSensors() {
@@ -183,6 +204,8 @@ public class Lab1 {
             sensors.add(new Point(6,11));
             sensors.add(new Point(15,13));
             sensors.add(new Point(15,11));
+            sensors.add(new Point(16,9));
+            sensors.add(new Point(3,9));
         }
         public void setTest(int speedtest) throws CommandException{
             int ny = (int) (Math.min(Math.abs(speed),speedtest) * Math.signum(speed));
