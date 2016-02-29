90577b5d393b02cfb5e5bf5f15afe123985a4a80
Erik Öhrn <erik.ohrn@outlook.com>
2016-02-29 13:44:48 +0100

    A bit better strucutured server code

--- cchat/server.erl
+++ cchat/server.erl
@@ -20,38 +20,32 @@ initial_state(ServerName) ->
 %% and NewState is the new state of the server.
 
 handle(St, {connect, UserState}) ->
-    io:fwrite("Server received in connect: ~p~n", [UserState]),
     CurrentUser = lists:keyfind(UserState#client_st.nick, #client_st.nick, St#server_st.connectedUsers),
     if CurrentUser == false -> Updated_St = St#server_st{connectedUsers = lists:append(St#server_st.connectedUsers, [UserState])},
     				{reply, ok, Updated_St};
-    			true -> {reply, user_already_connected, St}
+    	true -> {reply, user_already_connected, St}
 	end;
 
 handle(St, {disconnect, UserState}) ->
-	io:fwrite("Server received in disconnect: ~p~n", [UserState]),
 	{reply, ok, St#server_st{connectedUsers = lists:delete(UserState, St#server_st.connectedUsers)}};
 
 handle(St, {join, User, Channel}) ->
-io:fwrite("Server received in join: ~p~n", [Channel]),
-ChannelAtom = list_to_atom(Channel),
+	ChannelAtom = list_to_atom(Channel),
 	ChannelPID = whereis(ChannelAtom),
 	if ChannelPID == undefined ->
-			genserver:start(ChannelAtom, channel:initial_state(Channel), fun channel:handle/2);		
-			% Registers a new channel process if the channel name is not already registered  
+		% Register a new channel process if the channel name is not already registered  
+		genserver:start(ChannelAtom, channel:initial_state(Channel), fun channel:handle/2);		
 		true -> channel_already_running
 	end,
 	NewState = St#server_st{channelList = lists:append(St#server_st.channelList, [ChannelAtom])},  %% Kan man verkligen göra såhär?
-	Data = {join, User},
-	Response = genserver:request(list_to_atom(Channel), Data),
+	Response = genserver:request(list_to_atom(Channel), {join, User}),
     {reply, Response, NewState};
 
 handle(St, {leave, User, Channel}) ->
-	io:fwrite("Server received in leave: ~p~n", [Channel]),	% TODO debug
-	case  lists:member(list_to_atom(Channel),St#server_st.channelList) of
+	case lists:member(list_to_atom(Channel),St#server_st.channelList) of
 		false ->
 			{reply, channel_not_found, St};
 	 	true ->
-	 		Data = {leave, User},
-			Response = genserver:request(list_to_atom(Channel), Data),
+			Response = genserver:request(list_to_atom(Channel), {leave, User}),
 			{reply, Response, St}                   
     end.
