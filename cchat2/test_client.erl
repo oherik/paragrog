d452dc11c4b59b8d04e33ca8c68bc3961ea0484a
Erik Öhrn <erik.ohrn@outlook.com>
2016-03-02 13:37:14 +0100

    Updated skeleton code

--- cchat2/test_client.erl
+++ cchat2/test_client.erl
@@ -1,4 +1,404 @@
-ssert_ok(to_string(_ClientAtom1)++" pings "++_Nick2, Result2),
+-module(test_client).
+-include_lib("./defs.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+-define(SERVER,"server"). % forces students not to hardcode "shire"
+-define(SERVERATOM, list_to_atom(?SERVER)).
+-define(MAX, 100000).
+
+-define(PERF_1_USERS, 50).
+
+-define(PERF_2_USERS, 10).
+-define(PERF_2_CHANS, 100).
+-define(PERF_2_MSGS, 5).
+
+% --- Output -----------------------------------------------------------------
+
+% Turn output off/on
+is_output_off() ->
+    get(output) == off.
+output_off() ->
+    put(output,off).
+output_on() ->
+    put(output,on).
+
+putStrLn(S) ->
+    case get(output) of
+        off -> ok ;
+        _   -> io:fwrite(user, <<"~s~n">>, [S])
+    end.
+
+putStrLn(S1, S2) ->
+    putStrLn(io_lib:format(S1, S2)).
+sprintf(S1, S2) ->
+    lists:flatten(io_lib:format(S1, S2)).
+
+% To turn off colours, just use this function:
+% colour(Num,S) -> S.
+colour(Num,S) ->
+    "\033["++Num++"m"++S++"\033[0m".
+red(S) ->
+    colour("31",S).
+green(S) ->
+    colour("32",S).
+blue(S) ->
+    colour("34",S).
+gray(S) ->
+    colour("37",S).
+purple(S) ->
+    colour("35",S).
+
+% --- Helpers: assertions ----------------------------------------------------
+
+% Positive assertion, with error message
+assert(Message, Condition) ->
+    Pfx = Message++": ",
+    case (catch(?assert(Condition))) of
+        {'EXIT', _Ex} ->
+            Msg = Pfx++red("Fail"),
+            case get(output) of
+                off -> throw(Msg) ;
+                _   -> putStrLn(Msg), throw("Test failed")
+            end ;
+        _ -> putStrLn(Pfx++green("Ok"))
+    end.
+assert(Message, X, Y) ->
+    Pfx = Message++": ",
+    case (catch(?assertEqual(Y, X))) of
+        {'EXIT', _Ex} ->
+            Msg = Pfx++red("Fail")++
+                  sprintf("~nExpected: ~p~n     Got: ~p~n", [Y,X]),
+            case get(output) of
+                off -> throw(Msg) ;
+                _   -> putStrLn(Msg), throw("Test failed")
+            end ;
+        _ -> putStrLn(Pfx++green("Ok"))
+    end.
+assert_ok(X) ->
+    ?assertEqual(ok, X).
+assert_ok(Message, X) ->
+    assert(Message, X, ok).
+
+% Assert for particular error message
+assert_error(Result, Atom) ->
+    ?assertMatch({error, Atom, _}, Result).
+assert_error(Message, Result, Atom) ->
+    Pfx = Message++" fails: ",
+    case (catch(assert_error(Result, Atom))) of
+        {'EXIT', _Ex} ->
+            Msg = Pfx++red("Passes")++
+                  sprintf("~nExpected: {error,~p,_}~n     Got: ~p~n", [Atom,Result]),
+            case get(output) of
+                off -> throw(Msg) ;
+                _   -> putStrLn(Msg), throw("Test failed")
+            end ;
+        _ -> putStrLn(Pfx++green("Ok"))
+    end.
+
+% --- Helpers ----------------------------------------------------------------
+
+% Our own version of helper:request without a timeout
+request(Pid, Data) ->
+    Ref = make_ref(),
+    Pid!{request, self(), Ref, Data},
+    receive
+    {result, Ref, Result} ->
+        Result;
+    {exit, Ref, Reason} ->
+        exit(Reason)
+    end.
+
+% Generic to_string
+to_string({_Atom,Node}) ->
+    atom_to_list(Node);
+to_string(X) ->
+    io_lib:format("~p", [X]).
+
+% It is assumed that is called at the beginning of each test case (only)
+init(Name) ->
+    ?assert(compile:file(server) =:= {ok,server}),
+    putStrLn(blue("\n# Test: "++Name)),
+    InitState = server:initial_state(?SERVER),
+    Pid = genserver:start(?SERVERATOM, InitState, fun server:handle/2),
+    % putStrLn("server ~p", [Pid]),
+    assert("server startup", is_pid(Pid)),
+    Pid.
+
+% Start new GUI and register it as Name
+new_gui(Name) ->
+    new_gui(Name, self()).
+new_gui(Name, Mirror) ->
+    catch(unregister(list_to_atom(Name))),
+    {ok, Pid} = dummy_gui:start_link(Name,Mirror),
+    Pid.
+
+find_unique_name(Prefix) ->
+    MStr = integer_to_list(random:uniform(?MAX)),
+    Name = Prefix++MStr,
+    case whereis(list_to_atom(Name)) of
+        undefined -> Name ;
+        _         -> find_unique_name(Prefix)
+    end.
+
+% Start a new client
+new_client() ->
+    Nick = find_unique_name("user_"),
+    new_client(Nick).
+
+% Start a new client with a given nick
+new_client(Nick) ->
+    GUIName = find_unique_name("gui_"),
+    new_client(Nick, GUIName).
+
+% Start a new client with a given nick and GUI name
+new_client(Nick, GUIName) ->
+    ClientName = find_unique_name("client_"),
+    ClientAtom = list_to_atom(ClientName),
+    Pid = genserver:start(ClientAtom, client:initial_state(Nick, GUIName), fun client:handle/2),
+    {Pid, Nick, ClientAtom}.
+
+% Start a new client and connect to server
+new_client_connect() ->
+    {Pid, Nick, ClientAtom} = new_client(),
+    Result = request(ClientAtom, {connect, ?SERVER}),
+    assert_ok(to_string(ClientAtom)++" connects to server as "++Nick, Result),
+    {Pid, Nick, ClientAtom}.
+
+% Start a new client and connect to server
+new_client_connect(GUI) ->
+    case GUI of
+        true ->
+            Nick = find_unique_name("user_"),
+            GUIName = find_unique_name("gui_"),
+            new_gui(GUIName),
+            {Pid, Nick, ClientAtom} = new_client(Nick, GUIName),
+            Result = request(ClientAtom,{connect, ?SERVER}),
+            assert_ok(to_string(ClientAtom)++" connects to server as "++Nick, Result),
+            {Pid, Nick, ClientAtom}
+            ;
+        _ -> new_client_connect()
+    end.
+
+% Connect and assert it succeeded
+connect(ClientAtom) ->
+    Result = request(ClientAtom,{connect, ?SERVER}),
+    assert_ok(to_string(ClientAtom)++" connects to server", Result).
+
+% Join a channel and assert it succeeded
+join_channel(ClientAtom, Channel) ->
+    Result = request(ClientAtom,{join,Channel}),
+    assert_ok(to_string(ClientAtom)++" joins "++Channel, Result).
+
+% Leave a channel and assert it succeeded
+leave_channel(ClientAtom, Channel) ->
+    Result = request(ClientAtom,{leave,Channel}),
+    assert_ok(to_string(ClientAtom)++" leaves "++Channel, Result).
+
+% Disconnect and assert it succeeded
+disconnect(ClientAtom) ->
+    Result = request(ClientAtom,disconnect),
+    assert_ok(to_string(ClientAtom)++" disconnects from server", Result).
+
+% Send a message from GUI to client and assert it succeeded
+send_message(ClientAtom, Channel, Message) ->
+    Result = request(ClientAtom, {msg_from_GUI,Channel,Message}),
+    assert_ok(to_string(ClientAtom)++" sends message on "++Channel, Result).
+
+% Receive a specific message from dummy GUI
+receive_message(Channel, Nick, Message) ->
+    receive
+        {msg_to_GUI, From, Msg} ->
+            assert("channel matches", From, Channel),
+            assert("message matches", Msg, Nick++"> "++Message)
+    after
+        5000 ->
+            putStrLn(red("nothing received")),
+            ?assert(false)
+    end.
+
+% Make sure thare are no pending messages from dummy GUI
+no_more_messages() ->
+    receive
+        {msg_to_GUI, _From, _Msg} ->
+            putStrLn(red("there are unreceived messages")),
+            ?assert(false)
+    after
+        1000 ->
+            assert("no more messages", true)
+    end.
+
+% Get a new channel name
+new_channel() ->
+    find_unique_name("#channel_").
+
+% --- Good unit tests --------------------------------------------------------
+
+% A normal sequence of events
+normal_one_user_test() ->
+    init("normal_one_user"),
+    Channel = new_channel(),
+    {_Pid1, _Nick1, ClientAtom1} = new_client_connect(),
+    join_channel(ClientAtom1, Channel),
+    send_message(ClientAtom1, Channel, "hello"),
+    send_message(ClientAtom1, Channel, "goodbye"),
+    leave_channel(ClientAtom1, Channel),
+    disconnect(ClientAtom1).
+
+% One user writes, the other receives
+write_receive_test() ->
+    init("write_receive"),
+    Channel = new_channel(),
+
+    % Client 1
+    {_Pid1, Nick1, ClientAtom1} = new_client_connect(),
+    join_channel(ClientAtom1, Channel),
+
+    % Client 2 with dummy GUI
+    {_Pid2, _Nick2, ClientAtom2} = new_client_connect(true),
+    join_channel(ClientAtom2, Channel),
+
+    % Client 1 writes to to channel
+    Message = find_unique_name("message_"),
+    send_message(ClientAtom1, Channel, Message),
+
+    % Client 2 receives
+    receive_message(Channel, Nick1, Message),
+
+    % Client 2 leaves, 1 resends, no receive
+    leave_channel(ClientAtom2, Channel),
+    send_message(ClientAtom1, Channel, Message),
+    no_more_messages(),
+
+    ok.
+
+% Write/receive with multiple channels
+%
+%  User1    User2    User3
+%   | \     / | \     / |
+%   |  Chan1  |  Chan2  |
+%    \        |        /
+%     `---- Chan3 ----`
+write_receive_2_test() ->
+    init("write_receive_2"),
+    Channel1 = new_channel(),
+    Channel2 = new_channel(),
+    Channel3 = new_channel(),
+
+    % Client 1 -> Channel 1, 3
+    {_Pid1, Nick1, ClientAtom1} = new_client_connect(true),
+    join_channel(ClientAtom1, Channel1),
+    join_channel(ClientAtom1, Channel3),
+
+    % Client 2 -> Channel 1, 2, 3
+    {_Pid2, Nick2, ClientAtom2} = new_client_connect(true),
+    join_channel(ClientAtom2, Channel1),
+    join_channel(ClientAtom2, Channel2),
+    join_channel(ClientAtom2, Channel3),
+
+    % Client 3 -> Channel 2, 3
+    {_Pid3, Nick3, ClientAtom3} = new_client_connect(true),
+    join_channel(ClientAtom3, Channel2),
+    join_channel(ClientAtom3, Channel3),
+
+    % Client 1 writes to channel 1
+    % Receive from Client 2
+    Message1 = find_unique_name("message_"),
+    send_message(ClientAtom1, Channel1, Message1),
+    receive_message(Channel1, Nick1, Message1),
+
+    % Client 3 writes to channel 2
+    % Receive from Client 2
+    Message2 = find_unique_name("message_"),
+    send_message(ClientAtom3, Channel2, Message2),
+    receive_message(Channel2, Nick3, Message2),
+
+    % Client 2 writes to channel 3
+    % Receive from Client 1 and 2
+    Message3 = find_unique_name("message_"),
+    send_message(ClientAtom2, Channel3, Message3),
+    receive_message(Channel3, Nick2, Message3),
+    receive_message(Channel3, Nick2, Message3),
+
+    no_more_messages(),
+    ok.
+
+% Changing nick
+change_nick_test_DISABLED() ->
+    init("change_nick"),
+    Channel = new_channel(),
+
+    % Client 1
+    {_Pid1, _Nick1, ClientAtom1} = new_client_connect(),
+    join_channel(ClientAtom1, Channel),
+
+    % Client 2
+    {_Pid2, _Nick2, ClientAtom2} = new_client_connect(true),
+    join_channel(ClientAtom2, Channel),
+
+    % Change nick of 1
+    NewNick = find_unique_name("user_"),
+    Result = request(ClientAtom1, {nick,NewNick}),
+    assert_ok(to_string(ClientAtom1)++" changes nick to "++NewNick, Result),
+
+    % Client 1 writes to channel
+    % Make sure prompt in 2 reflects correct name
+    Message = find_unique_name("message_"),
+    send_message(ClientAtom1, Channel, Message),
+    receive_message(Channel, NewNick, Message),
+
+    ok.
+
+% Combined test for changing nick
+change_nick_combined_test() ->
+    init("change_nick_combined"),
+    Channel = new_channel(),
+
+    % Client 1
+    {_Pid1, _Nick1, ClientAtom1} = new_client_connect(),
+    join_channel(ClientAtom1, Channel),
+
+    % Client 2
+    {_Pid2, Nick2, ClientAtom2} = new_client_connect(true),
+    join_channel(ClientAtom2, Channel),
+
+    % Change nick of 1 to something unique
+    NewNick = find_unique_name("user_"),
+    Result = request(ClientAtom1, {nick,NewNick}),
+    case Result of
+        % Client supports online nick change
+        ok ->
+            % Client 1 writes to channel
+            % Make sure prompt in 2 reflects correct name
+            Message = find_unique_name("message_"),
+            send_message(ClientAtom1, Channel, Message),
+            receive_message(Channel, NewNick, Message),
+
+            % Change nick of 1 to 2
+            Result2 = request(ClientAtom1,{nick,Nick2}),
+            assert_error(to_string(ClientAtom1)++" changing nick to "++Nick2, Result2, nick_taken) ;
+
+        % Client doesn't support online nick change
+        {error, user_already_connected, _} -> ok
+    end.
+
+% Ping test (not run automatically)
+ping() ->
+    init("ping"),
+
+    % Client 1
+    {_Pid1, _Nick1, _ClientAtom1} = new_client_connect(true),
+
+    % Send ping to non-existent user
+    BadNick = "smeagol",
+    Result1 = request(_ClientAtom1, {ping,BadNick}),
+    assert_error(to_string(_ClientAtom1)++" pings "++BadNick, Result1, user_not_found),
+
+    % Client 2
+    {_Pid2, _Nick2, _ClientAtom2} = new_client_connect(),
+
+    % Send ping from 1 to 2
+    Result2 = request(_ClientAtom1, {ping,_Nick2}),
+    assert_ok(to_string(_ClientAtom1)++" pings "++_Nick2, Result2),
 
     % Make sure pong is received
     % Don't check message format, since students may change it
