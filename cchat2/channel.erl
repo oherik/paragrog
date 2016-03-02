a39995398b8c551c40049cd4d40c5913c4799248
Erik Öhrn <erik.ohrn@outlook.com>
2016-03-02 13:28:44 +0100

    Added lab4 skeleton

---
+++ cchat2/channel.erl
@@ -0,0 +1,55 @@
+-module(channel).
+-export([handle/2, initial_state/1, spawnMessage/4]).
+-include_lib("./defs.hrl").
+
+%%	initial_state/1
+
+%	Produces the initial state
+initial_state(ChannelName) ->
+	#channel_st{channelName = ChannelName}.
+
+%%	handle/2
+
+% 	Arguments:
+%		St: the current state for the channel
+%
+%	Handles the various requests from the server and clients
+%
+% 	Returns: a tuple consisting of 'reply', 'ok' and the updated channel state
+
+% 	Adds the user's pid to the list of connected users. The check if the user is already connected is 
+%	assumed to be handled in the client.
+handle(St, {join, UserPid}) ->
+	{reply, ok, St#channel_st{connectedUsers = lists:append(St#channel_st.connectedUsers, [UserPid])}};
+
+% 	Removes the user's pid from the list of connected users. The check if the user is connected is 
+%	assumed to be handled in the client.
+handle(St, {leave, UserPid}) ->
+	{reply, ok, St#channel_st{connectedUsers = lists:delete(UserPid, St#channel_st.connectedUsers)}};
+
+% 	Retrieves all the connected users' pids where the pid is not the same as the sender's. This is done 
+% 	to make sure that the sender doesn't send its message to itself. When that is done the list is used
+%	to create a pmap, which in turn starts the spawnMessage function for all the contained pids. As 
+%	additional arguments it sends the channel name, the sender's nick and the message itself
+handle(St, {msg_from_GUI, UserNick, UserPid, Message}) ->
+	ConnectedPids = lists:delete(UserPid, St#channel_st.connectedUsers),
+	rpc:pmap({channel, spawnMessage}, [St#channel_st.channelName,UserNick, Message], ConnectedPids),
+	{reply, ok, St}.
+
+
+%%	spawnMessage/4
+
+%	Used in combination with the other spawnMessage method when the latter is to be used in
+%	conjunction with a pmap. This is needed for the case where there are no clients left to
+%	sen the message to, as indicated by the empty list argument.
+spawnMessage([],_,_,_) -> no_client;
+
+%	Creates a new process which spawns a genserver request in order to send a message to a client. 
+%	The process creation is needed in order to avoid deadlock, which would occur if the function
+%	simply made a request without starting a new process.
+%
+%	Returns: the pid for the created process.
+spawnMessage(Client, Channel, UserNick, Message) -> spawn (fun () ->
+	genserver:request(Client, {incoming_msg, Channel, UserNick, Message})
+end).
+
---
+++ cchat2/server.erl
@@ -0,0 +1,67 @@
+-module(server).
+-export([handle/2, initial_state/1]).
+-include_lib("./defs.hrl").
+
+%% initial_state/2 and handle/2 are used together with the genserver module,
+%% explained in the lecture about Generic server.
+
+% Produce initial state
+initial_state(ServerName) ->
+	#server_st{serverName = ServerName}. 
+
+%% ---------------------------------------------------------------------------
+
+%% handle/2 handles requests from clients
+
+%% All requests are processed by handle/2 receiving the request data (and the
+%% current state), performing the needed actions, and returning a tuple
+%% {reply, Reply, NewState}, where Reply is the reply to be sent to the client
+%% and NewState is the new state of the server.
+
+%	Arguments:
+%		User: the nick of the client
+%	
+%	Adds the user's nick to the list of connected users, if it's not already connected. If it is connected,
+%	it replies with and user_already_connected message.
+handle(St, {connect, User}) ->
+case lists:member(User, St#server_st.connectedUsers) of
+	false ->
+    	{reply, ok, St#server_st{connectedUsers = lists:append(St#server_st.connectedUsers, [User])}};
+    true ->
+    	{reply, user_already_connected , St}
+    end;
+
+%	Arguments:
+%		User: the nick of the client
+%	
+%	Removes the user's nick from the list of connected users.
+handle(St, {disconnect, User}) ->
+	{reply, ok, St#server_st{connectedUsers = lists:delete(User, St#server_st.connectedUsers)}};
+
+%	Arguments:
+%		UserPid: the connecting client's pid
+%		Channel: the channel the user wishes to connect to
+%	
+%	First, the function checks if the channel is already started. In order to mantain the property that
+%	several server should be able to have channels with the same name, a new channel is started and 
+%	registered in genserver using a combination of the server name and the channel name. This is 
+%	possible, since the combination (server name, channel name) is unique. If this was left out, it
+%	would not be possible to have ServerA with a channel #Channel as well as ServerB with a channel 
+% 	#Channel. Thus, a full channel atom is created, which is the server's name followed by the channel's 
+%	name.
+%
+%	A request is then sent to the server running and registered with this full name atom, which calls
+%	for its join method.
+%
+%	The checks if the channel is valid to join for this particular user is made in the client process in
+%	order to avoid server bottlenecks. However, this method is written here in the server process since 
+%	starting channels via the channel was a specification in the lab PM. 
+handle(St, {join, UserPid, Channel}) ->
+	FullChannelAtom = list_to_atom(St#server_st.serverName ++ Channel),
+	ChannelPID = whereis(FullChannelAtom),
+	if 	ChannelPID == undefined ->
+			% Register a new channel process if the channel name is not already registered  
+			genserver:start(FullChannelAtom, channel:initial_state(Channel), fun channel:handle/2);		
+		true -> channel_already_running
+	end,
+    {reply,	genserver:request(FullChannelAtom, {join, UserPid}),St}.
